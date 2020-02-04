f351d57ff9b39db718af640a4abc63a66980ace2
Joey Register <josephregister1258@gmail.com>
2020-02-03 21:54:13 -0500

    server created and routing to homepage

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const path = require("path")
 const app = express()
 const PORT = process.env.PORT||3030
 
-const htmlRoute = "/routes/"
+const htmlRoute = path.join(__dirname,"/routes","/")
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get("/", (_,res)=>{
+    res.sendFile(htmlRoute+"index.html")
+})
 
 app.listen(PORT,()=>{
     console.log("NotePad 2.0")
