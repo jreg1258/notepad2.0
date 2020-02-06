b0f2d080fd68d416066a4a1e5d6f0c931c33f920
Joey Register <josephregister1258@gmail.com>
2020-02-06 17:33:57 -0500

    edits to code, added css.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const path = require("path")
+//const notefn = require("./assets/js/index")
+
 
 const app = express()
 const PORT = process.env.PORT||3030
@@ -10,9 +12,26 @@ const htmlRoute = path.join(__dirname,"/routes","/")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const notes = []
+
 
 app.get("/", (_,res)=>{
     res.sendFile(htmlRoute+"index.html")
+})
+
+app.get("/notes", (_,res)=>{
+    res.sendFile(htmlRoute+"notes.html")
+})
+
+app.get("/api/notes", (_,res)=>{
+    res.json()
+})
+
+app.post("/api/notes", (req,res)=>{
+    const savedNote = req.body
+
+    notes.push(savedNote)
+    res.send(200)
 })
 
 app.listen(PORT,()=>{
